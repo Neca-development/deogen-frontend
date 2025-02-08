50592cdb06f08fdeffa4b0aa7b57528c6d42b2e3
VladislavPH <vpuzyrev@unistory.app>
2025-02-08 15:16:58 +0300

    npm tanstack react-query

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,12 +1,17 @@
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <MantineProvider>
-      <RouterProvider router={router} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
+    </QueryClientProvider>
   )
 }
