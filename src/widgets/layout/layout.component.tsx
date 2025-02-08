5bfe13aa1254eb14f6dfeacf0db0361956761ea4
VladislavPH <vpuzyrev@unistory.app>
2025-02-08 15:18:01 +0300

    update layout

---
+++ src/widgets/layout/layout.component.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx'
+import { Footer, Header, Main } from './ui'
+
+type LayoutProps = React.ComponentPropsWithoutRef<'div'>
+
+export function Layout(props: LayoutProps) {
+  const { className, children, ...otherProps } = props
+
+  return (
+    <div className={clsx('grid min-h-dvh grid-rows-layout', className)} {...otherProps}>
+      {children}
+    </div>
+  )
+}
+
+Layout.Header = Header
+Layout.Main = Main
+Layout.Footer = Footer
