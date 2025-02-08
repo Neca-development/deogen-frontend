6cfb6be4797a8f1e72663d958540b7e3ae4a46e5
VladislavPH <vpuzyrev@unistory.app>
2025-02-08 15:18:28 +0300

    update

---
+++ src/pages/home/page.tsx
@@ -0,0 +1,11 @@
+import { Layout } from '@widgets/layout'
+
+export function HomePage() {
+  return (
+    <Layout>
+      <Layout.Header />
+      <Layout.Main />
+      {/* <Layout.Footer /> */}
+    </Layout>
+  )
+}
